b7a7925a7b598350e96542616a23f977ee3e5b7f
Goran Stevanovic <stevanovicgoran@proton.me>
2022-08-03 10:18:16 +0200

    Set up variables

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// initial values
+const initialAmount = 42;
+const initialDecimals = 2;
+
+// form elements
+const amountInput = document.getElementById('input');
+const convertBtn = document.getElementById('convert');
+
+// input value elements
+const inputMetersEl = document.getElementById('input-meters');
+const inputFeetEl = document.getElementById('input-feet');
+const inputLitersEl = document.getElementById('input-liters');
+const inputGallonsEl = document.getElementById('input-gallons');
+const inputKilogramsEl = document.getElementById('input-kilograms');
+const inputPoundsEl = document.getElementById('input-pounds');
+
+// output values elements
+const outputFeetEl = document.getElementById('output-feet');
+const outputMetersEl = document.getElementById('output-meters');
+const outputGallonsEl = document.getElementById('output-gallons');
+const outputLitersEl = document.getElementById('output-liters');
+const outputPoundsEl = document.getElementById('output-pounds');
+const outputKilogramsEl = document.getElementById('output-kilograms');
